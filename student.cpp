905b851ed94ea3feb3a8eda26f21de1c58071463
MayurTipras <123481461+MayurTipras@users.noreply.github.com>
2023-10-21 15:11:39 +0530

    Add files via upload

---
+++ student.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class Student(
+		   		int id,m1,m2,m3;
+   				string name;
+   				 
+   		public : 
+   				 Student()
+				{
+   				 	cout<<"Default "
+				}
+   				 Student(int id, int m1, int m2, int m3, string name)
+   				 {
+   				 	this->id=id;
+   				 	this->m1=m1;
+   				 	this->m2=m2;
+   				 	this->m3=m3;
+   				 	this->name=name;
+   				 	
+				}
+				
+				void percentage()
+				{
+					int total;
+					int percent;
+					total=this->m1+this->m2+this->m3;
+					percent= ((total*100)/300);
+					cout<<"Percentage =" percent;
+				}
+   				
+);
+int main()
+{
+	Student s;
+	
+	
+}
